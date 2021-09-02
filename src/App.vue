437f158ac74043a4d250e52a9c208c637df6db69
lucky500 <luciano101@msn.com>
2021-09-02 16:59:30 -0400

    add Door Component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="monty-hall">
-    <Gift />
+    <!-- <Gift /> -->
+  </div>
+  <div class="doors">
+    <Door number="1" hasGift="true" />
+    <Door number="2" hasGift="false" />
+    <Door number="3" hasGift="false" />
   </div>
 </template>
 
 <script>
-import Gift from "./components/Gift";
+// import Gift from "./components/Gift";
+import Door from "./components/Door";
 
 export default {
   name: 'App',
   components: {
-    Gift
+    // Gift,
+    Door
   }
 }
 </script>
 
 <style>
+:root {
+  --frame-border: 5px solid #c77c1a;
+}
+
 body {
   background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+}
+
+.doors {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
---
+++ src/components/Door.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="area">
+      <div class="frame">
+          <Gift />
+      </div>
+      <div class="door">
+          <div class="number">{{ number }}</div>
+          <div class="knob"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Gift from './Gift';
+
+export default {
+    props: {
+        number: { type: Number },
+        hasGift: { type: Boolean },
+    },
+    components:  {
+        Gift
+    }
+}
+</script>
+
+<style scoped>
+.area {
+    position: relative;
+    width: 200px;
+    height: 310px;
+    background-color: #8f5201;
+    font-size: 3rem;
+}
+
+.frame {
+    position: absolute;
+    height: 300px;
+    width: 180px;
+
+    border-left: var(--frame-border);
+    border-top: var(--frame-border);
+    border-right: var(--frame-border);
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+
+}
+</style>
